dbba76030b445838c8a22c8b05e35b30f3530f40
benyaminaishak80-tech <benyaminaishak80@gmail.com>
2026-06-28 19:05:13 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const mineflayer = require('mineflayer');
+
+function createBot() {
+    const bot = mineflayer.createBot({
+        host: 'IHKBN31.aternos.me', 
+        port: 37933,                
+        username: 'Bot__ISHAK'
+        // تنحى سطر الـ version باش البوت يجيبها وحدو تلقائياً
+    });
+
+    bot.on('spawn', () => {
+        console.log('البوت دخل للسيرفر بنجاح!');
+        
+        // Anti-AFK: البوت يقفز كل 30 ثانية باش ما يطردوش السيرفر
+        setInterval(() => {
+            bot.setControlState('jump', true);
+            setTimeout(() => bot.setControlState('jump', false), 500);
+        }, 30000);
+    });
+
+    bot.on('end', () => {
+        console.log('البوت خرج. جاري إعادة الاتصال بعد 30 ثانية...');
+        setTimeout(createBot, 30000);
+    });
+
+    bot.on('error', (err) => {
+        console.log('صرا خطأ في الاتصال: ', err.message);
+    });
+}
+
+createBot();
